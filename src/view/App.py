7066d23736b18b822b24e49397784db124a44fa0
dankocaboski <caboskidanko@gmail.com>
2024-06-20 11:03:25 -0300

    CustomCheckbox

--- src/view/App.py
+++ src/view/App.py
@@ -1,6 +1,9 @@
 import customtkinter
+
+from components.CustomCheckbox import CustomCheckbox
 from components.CustomButton import CustomButton
-from src.view.functions.ButtonHandler import ButtonHandler
+
+from functions.ButtonHandler import ButtonHandler
 
 
 class App(customtkinter.CTk):
@@ -15,10 +18,13 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure((0, 1), weight=1)
 
-        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox 1")
-        self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
-        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+        inst_custom_checkbox_1 = CustomCheckbox(self, "Checkbox 1")  # Create an instance of CustomCheckbox
+        checkbox_1 = inst_custom_checkbox_1()  # Call the instance to create the checkbox
+        checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")  # Add the checkbox to the grid
+        
+        inst_custom_checkbox_2 = CustomCheckbox(self, "Checkbox 2")
+        checkbox_2 = inst_custom_checkbox_2()
+        checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
 
         custom_button = CustomButton(self, "Meu botão", command=self.callback_handler.button_callback)  # Create an instance of CustomButton
         button = custom_button()  # Call the instance to create the button
---
+++ src/view/components/CustomCheckbox.py
@@ -0,0 +1,10 @@
+import customtkinter
+
+class CustomCheckbox:
+    def __init__(self, frame, texto):
+        self.frame = frame
+        self.texto = texto
+
+    def __call__(self):
+        checkbox = customtkinter.CTkCheckBox(self.frame, text=self.texto)
+        return checkbox
