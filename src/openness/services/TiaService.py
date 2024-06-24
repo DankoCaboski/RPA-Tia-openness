a8115cefc774b349489bec8f7b8595f148e1c7c3
dankocaboski <caboskidanko@gmail.com>
2024-06-24 14:40:26 -0300

    Feat: get_all_devices

--- src/openness/services/TiaService.py
+++ src/openness/services/TiaService.py
@@ -4,6 +4,7 @@ class TiaService:
     def __init__(self, tia):
         self.tia = tia
         self.tia_instance = None
+        self.myproject = None
         
     def open_tia_ui(self):
         # Create an instance of Tia Portal
@@ -14,4 +15,18 @@ class TiaService:
         if self.tia_instance is None:
             self.open_tia_ui()
         proj_path = Utils().get_directory_info(proj_path+"\\"+proj_name)
-        self.tia_instance.Projects.Create(proj_path, proj_name)
+        self.myproject = self.tia_instance.Projects.Create(proj_path, proj_name)
+        
+    def get_all_devices(self):
+        try:
+            devices = []
+            for device in self.myproject.Devices:
+                devices.append(device)
+            return devices
+        except Exception as e:
+            print('Error getting all devices:', e)
+            
+    def get_device_by_index(self, index):
+        cpu_list = self.get_all_devices(self.myproject)
+        cpu = cpu_list[index]
+        return cpu 
