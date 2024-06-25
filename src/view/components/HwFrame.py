fce52b1c7ad4810bc3309b8d4b5807289034c71e
dankocaboski <caboskidanko@gmail.com>
2024-06-25 17:21:28 -0300

    Fix: btn_add_hw

--- src/view/components/HwFrame.py
+++ src/view/components/HwFrame.py
@@ -1,5 +1,6 @@
 import customtkinter
 import tkinter as tk
+from tkinter import ttk
 from openness.services.Utils import Utils
 
 class HwFrame:
@@ -9,7 +10,8 @@ class HwFrame:
         self.frame.grid_columnconfigure(0, weight=1)
         self.frame.grid_columnconfigure(1, weight=0)
         self.frame.grid_columnconfigure(2, weight=0)
-        self.frame.grid_columnconfigure(3, weight=1)
+        self.frame.grid_columnconfigure(3, weight=0)
+        self.frame.grid_columnconfigure(4, weight=1)
         
         self.opcoes_Hardware = ["PLC", "IHM", "IO Node"]
         
@@ -23,31 +25,42 @@ class HwFrame:
         self.add_hw()
     
     def add_hw(self):
+        global btn_add_hw
         btn_add_hw = customtkinter.CTkButton(self.frame, text="Adicionar Hardware", command=self.add_hw_combobox)
-        btn_add_hw.grid(row=0, column=1, columnspan=2, padx=10, pady=10)
+        btn_add_hw.grid(row=0, column=1, columnspan=2, pady=10)
         
     def add_hw_combobox(self):                
         input = {
             "combobox": tk.StringVar(),
             "mlfb": tk.StringVar(),
             "firm_version": tk.StringVar(),
-            "entry": tk.StringVar()
+            "entry": tk.StringVar(),
+            "Start_Adress": tk.StringVar()
             }
         
         self.hardware_values.append(input)
         
+        def focus_next_widget(event):
+            event.widget.tk_focusNext().focus()
+            return "break"
         
-        type_hw = customtkinter.CTkComboBox(self.frame, variable=input["combobox"], values=self.opcoes_Hardware)
-        type_hw.grid(row=self.row_counter, column=0, padx=10, pady=10)
+        type_hw = customtkinter.CTkComboBox(self.frame, width=120, variable=input["combobox"], values=self.opcoes_Hardware)
+        type_hw.grid(row=self.row_counter, column=0, padx=(8,0), pady=10, sticky='e')
         
         hw_mlfb = customtkinter.CTkComboBox(self.frame, variable=input["mlfb"])
-        hw_mlfb.grid(row=self.row_counter, column=1, padx=10, pady=10)
+        hw_mlfb.grid(row=self.row_counter, column=1, padx=(8,0), pady=10)
         
-        hw_firmware = customtkinter.CTkComboBox(self.frame, variable=input["firm_version"])
-        hw_firmware.grid(row=self.row_counter, column=2, padx=10, pady=10)
+        hw_firmware = customtkinter.CTkComboBox(self.frame, width=120, variable=input["firm_version"])
+        hw_firmware.grid(row=self.row_counter, column=2, padx=(8,0), pady=10)
         
-        hw_name = customtkinter.CTkEntry(self.frame, textvariable=input["entry"])
-        hw_name.grid(row=self.row_counter, column=3, padx=10, pady=10)
+        hw_name = customtkinter.CTkEntry(self.frame, width=120, textvariable=input["entry"])
+        hw_name.grid(row=self.row_counter, column=3, padx=(8,0), pady=10)
+        
+        special_entry = customtkinter.CTkEntry(self.frame, textvariable=input["Start_Adress"])
+        special_entry.grid(row=self.row_counter, column=4, padx=(8,0), pady=10)
+        
+        special_entry.bind('<Return>', focus_next_widget)
+        special_entry.grid_remove()  # Inicia sem aparecer
         
         def update_mlfb_combobox(*args):
             selected_option = input["combobox"].get()
@@ -55,10 +68,14 @@ class HwFrame:
             
             if selected_option == "PLC":
                 valueSource = self.mlfb_List[0]
+                special_entry.grid_remove()
             elif selected_option == "IHM":
                 valueSource = self.mlfb_List[1]
+                special_entry.grid_remove()
             elif selected_option == "IO Node":
                 valueSource = self.mlfb_List[2]
+                special_entry.grid()
+                btn_add_hw.grid(row=0, column=0, columnspan=5, pady=10)
             else:
                 valueSource = []
 
