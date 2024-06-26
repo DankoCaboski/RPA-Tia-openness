bf9943a05ef38129c7bffde76a1fe3c1466db9fe
dankocaboski <caboskidanko@gmail.com>
2024-06-26 17:58:40 -0300

    Chore: setup.py

--- src/setup.py
+++ src/setup.py
@@ -9,31 +9,31 @@ build_exe_options = {
         os.path.join("src", "view", "assets", "favico.ico"),
         os.path.join("src", "view", "assets", "homeIcoDark.png"),
         
-        os.path.join("src", "view", "components", "CustomButton.py"),
-        os.path.join("src", "view", "components", "CustomCheckbox.py"),
-        os.path.join("src", "view", "components", "HwFrame.py"),
+        # os.path.join("src", "view", "components", "CustomButton.py"),
+        # os.path.join("src", "view", "components", "CustomCheckbox.py"),
+        # os.path.join("src", "view", "components", "HwFrame.py"),
         
-        os.path.join("src", "view", "functions", "ButtonHandler.py"),
-        os.path.join("src", "view", "functions", "FrameManagement.py"),
-        os.path.join("src", "view", "functions", "HomeIcon.py"),
+        # os.path.join("src", "view", "functions", "ButtonHandler.py"),
+        # os.path.join("src", "view", "functions", "FrameManagement.py"),
+        # os.path.join("src", "view", "functions", "HomeIcon.py"),
         
-        os.path.join("src", "view", "pages", "CreateProject.py"),
-        os.path.join("src", "view", "pages", "HomePage.py"),
-        os.path.join("src", "view", "pages", "OpenProject.py"),
+        # os.path.join("src", "view", "pages", "CreateProject.py"),
+        # os.path.join("src", "view", "pages", "HomePage.py"),
+        # os.path.join("src", "view", "pages", "OpenProject.py"),
         
-        os.path.join("src", "openness", "controllers", "OpennessController.py"),
+        # os.path.join("src", "openness", "controllers", "OpennessController.py"),
         
         os.path.join("src", "openness", "database", "Openness.db"),
         
-        os.path.join("src", "openness", "repositories", "DbManagement.py"),
+        # os.path.join("src", "openness", "repositories", "DbManagement.py"),
         
-        os.path.join("src", "openness", "services", "CompilerService.py"),
-        os.path.join("src", "openness", "services", "HwFeaturesService.py"),
-        os.path.join("src", "openness", "services", "LanguageService.py"),
-        os.path.join("src", "openness", "services", "OponessService.py"),
-        os.path.join("src", "openness", "services", "TiaService.py"),
-        os.path.join("src", "openness", "services", "Utils.py"),
-        os.path.join("src", "openness", "services", "XmlService.py"),
+        # os.path.join("src", "openness", "services", "CompilerService.py"),
+        # os.path.join("src", "openness", "services", "HwFeaturesService.py"),
+        # os.path.join("src", "openness", "services", "LanguageService.py"),
+        # os.path.join("src", "openness", "services", "OponessService.py"),
+        # os.path.join("src", "openness", "services", "TiaService.py"),
+        # os.path.join("src", "openness", "services", "Utils.py"),
+        # os.path.join("src", "openness", "services", "XmlService.py"),
    
     ]
 }
@@ -45,12 +45,13 @@ target = Executable(
     base=base,
     script="src/App.py",
     icon="src/view/assets/favico.ico",
-    target_name="RPA Tia Openness.exe"
+    target_name="RPA Tia Openness"
     )
 
 setup(
     version="1.1.0",
     description="Interface for automated creation of TIA Portal projects using Openness API",
     options={"build_exe": build_exe_options},
-    executables=[target]
+    executables=[target],
+    include_msvcr=True
 )
