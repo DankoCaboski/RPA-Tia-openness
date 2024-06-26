c2c2d3acb7eaf33af5af684b7003c5bacbc241c5
dankocaboski <caboskidanko@gmail.com>
2024-06-26 10:27:25 -0300

    Fix: homeIco

--- src/view/functions/HomeIcon.py
+++ src/view/functions/HomeIcon.py
@@ -4,8 +4,8 @@ from PIL import Image
 class HomeIcon:
     def load_image(self):
         
-        image = customtkinter.CTkImage(light_image=Image.open("view/assets/home.png"),
-                                  dark_image=Image.open("view/assets/home.png"),
+        image = customtkinter.CTkImage(light_image=Image.open("view/assets/homeIcoDark.png"),
+                                  dark_image=Image.open("view/assets/homeIcoDark.png"),
                                   size=(18, 18))
         
         return image
