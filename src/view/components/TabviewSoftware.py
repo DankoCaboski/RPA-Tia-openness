7d745b65fdd09cafabc0e17cd7be85800a54bc6f
dankocaboski <caboskidanko@gmail.com>
2024-06-27 11:34:40 -0300

    Feat: entradas TabviewSoftware

--- src/view/components/TabviewSoftware.py
+++ src/view/components/TabviewSoftware.py
@@ -6,16 +6,32 @@ class TabviewSoftware:
     def __init__(self, sw_frame):
         
         frame1 = customtkinter.CTkFrame(sw_frame)
-        label1 = customtkinter.CTkLabel(frame1, text="rb")
-        label1.pack()
+        label_1 = customtkinter.CTkLabel(frame1, text="Robôs que deseja adicionar: ")
+        label_1.grid(row=0, column=0, sticky="e", padx=(0,10), pady=10)
+        
+        rb_entry = customtkinter.CTkEntry(frame1)
+        rb_entry.insert(0, "0")
+        rb_entry.grid(row=0, column=1, padx=(10,10), pady=10, sticky="w")
+        
+        ################### Mesa giratoria ###################
         
         frame2 = customtkinter.CTkFrame(sw_frame)
-        label2 = customtkinter.CTkLabel(frame2, text="ms")
-        label2.pack()
+        label_2 = customtkinter.CTkLabel(frame2, text="Mesas giratórias que deseja adicionar: ")
+        label_2.grid(row=1, column=0, sticky="e", padx=(0,10), pady=10)
+        
+        mg_entry = customtkinter.CTkEntry(frame2)
+        mg_entry.insert(0, "0")
+        mg_entry.grid(row=1, column=1, padx=(10,10), pady=10, sticky="w")
+        
+        ################### gp ###################
         
         frame3 = customtkinter.CTkFrame(sw_frame)
-        label3 = customtkinter.CTkLabel(frame3, text="gp")
-        label3.pack()
+        label_3 = customtkinter.CTkLabel(frame3, text="Grampos que deseja adicionar: ")
+        label_3.grid(row=2, column=0, sticky="e", padx=(0,10), pady=10)
+        
+        gp_entry = customtkinter.CTkEntry(frame3)
+        gp_entry.insert(0, "0")
+        gp_entry.grid(row=2, column=1, padx=(10,10), pady=10, sticky="w")
     
         
         self.frames = [frame1, frame2, frame3]
