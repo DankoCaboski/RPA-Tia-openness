921566b96bb976d6d6af0e6fcab551b4fcf9fbe9
dankocaboski <caboskidanko@gmail.com>
2024-07-05 11:35:03 -0300

    Fix typo

--- src/view/components/InputConveyor.py
+++ src/view/components/InputConveyor.py
@@ -4,6 +4,9 @@ from tkinter import BooleanVar
 class InputConveyor:
     def __init__(self, frame):
         self.frame = customtkinter.CTkFrame(frame, fg_color="transparent")
+        self.frame.grid_columnconfigure(0, weight=1)
+        self.frame.grid_columnconfigure(1, weight=1)
+        self.frame.grid_columnconfigure(2, weight=1)
         
         self.inversor = BooleanVar()
         self.sensor_presenca = BooleanVar()
@@ -14,17 +17,17 @@ class InputConveyor:
         
     def main_content(self):
         
-        lb_inversor = customtkinter.CTkLabel(self.frame, text="Intversor?")
-        lb_inversor.grid(row=0, column=0, padx=3, pady=3, sticky='ws')
-        inversor = customtkinter.CTkCheckBox(self.frame, variable=self.inversor)
-        inversor.grid(row=1, column=0, padx=3, pady=3, sticky='wn')
+        lb_inversor = customtkinter.CTkLabel(self.frame, text="Inversor?")
+        lb_inversor.grid(row=0, column=0, padx=3, pady=(3, 10), sticky='ws')
+        ck_inversor = customtkinter.CTkCheckBox(self.frame, text="", variable=self.inversor)
+        ck_inversor.grid(row=1, column=0, padx=3, pady=(3, 10), sticky='nsew')
         
         lb_s_presenca = customtkinter.CTkLabel(self.frame, text="Sensor de presença?")
-        lb_s_presenca.grid(row=0, column=1, padx=3, pady=3, sticky='ws')
-        s_presenca = customtkinter.CTkCheckBox(self.frame, variable=self.sensor_presenca)
-        s_presenca.grid(row=1, column=1, padx=3, pady=3, sticky='wn')
+        lb_s_presenca.grid(row=0, column=1, padx=3, pady=(3, 10), sticky='ws')
+        s_presenca = customtkinter.CTkCheckBox(self.frame, text="", variable=self.sensor_presenca)
+        s_presenca.grid(row=1, column=1, padx=3, pady=(3, 10), sticky='nsew')
         
         lb_s_pos = customtkinter.CTkLabel(self.frame, text="Sensor de posição?")
-        lb_s_pos.grid(row=0, column=2, padx=3, pady=3, sticky='ws')
-        s_pos = customtkinter.CTkCheckBox(self.frame, variable=self.sensor_posicao)
-        s_pos.grid(row=1, column=2, padx=3, pady=3, sticky='wn')
+        lb_s_pos.grid(row=0, column=2, padx=3, pady=(3, 10), sticky='ws')
+        s_pos = customtkinter.CTkCheckBox(self.frame, text="", variable=self.sensor_posicao)
+        s_pos.grid(row=1, column=2, padx=3, pady=(3, 10), sticky='nsew')
