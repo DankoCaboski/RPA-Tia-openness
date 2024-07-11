4013b486ee66bfe3b07a89fbc874387252ef775a
Gabriel Ribeiro <133721315+Gbrribeiro30@users.noreply.github.com>
2024-07-11 16:46:32 -0300

    Correções nas telas IHM

--- src/openness/services/IHMService.py
+++ src/openness/services/IHMService.py
@@ -14,36 +14,50 @@ class IHMService:
             deviceName = device['name']
             if deviceType == 'IHM':
                 device = self.tia_service.get_device_by_name(deviceName)
-                print("device: ", device)
                 ihm = self.tia_service.get_Software_IHM(device).Software
                 
-                #Criação dos Grupos da telas
+                #Criação dos Grupos da telas Sistemas
                 Sistemas =  '01_Sistemas'
                 system_group = self.tia_service.create_folder(device, Sistemas, None).Screens
+                group_name_axis =  '01.1_Axis Template'
+                Axis_group = self.tia_service.create_folder(device, group_name_axis, Sistemas).Screens
                 group_name_celula =  '01.2_Célula'
                 Celula_group = self.tia_service.create_folder(device, group_name_celula, Sistemas).Screens
-                group_name_prod_temp =  '1.2.1Produção e Tempo de Ciclo'
-                prod_group = self.tia_service.create_folder(device, group_name_prod_temp, group_name_celula).Screens
+                group_name_prod =  '1.2.1_Contagem de Produção'
+                prod_group = self.tia_service.create_folder(device, group_name_prod, group_name_celula).Screens
+                group_name_prod_temp =  '1.2.2_Tempo de Ciclo'
+                temp_group = self.tia_service.create_folder(device, group_name_prod_temp, group_name_celula).Screens
                 group_name_alarm =  '01.3_Alarmes'
                 alarm_group = self.tia_service.create_folder(device, group_name_alarm, Sistemas).Screens
                 
+                #Criação dos Grupos da telas Operacionais
+                opera =  '03_Telas Operacionais'
+                system_group = self.tia_service.create_folder(device, opera, None).Screens
+                robos = '3.4_Robôs'
+                robos_group = self.tia_service.create_folder(device, robos, opera).Screens
+                robo1 = '3.4.1_RB01'
+                rb1_group = self.tia_service.create_folder(device, robo1, robos).Screens
+
                 #Criação dos grupos das tags
                 Sistemas_tag =  '01_Sistemas'
                 system_group_tag = self.tia_service.create_folder_tag(device, Sistemas_tag, None).TagTables
-                group_name_celula =  '01.2_Célula'
 
                 #Chamadas
                 self.import_template(ihm)
-                self.import_Screens(system_group, 'sistema')
+                self.import_Screens(Axis_group, 'sistema')
                 self.import_Screens(prod_group, 'prod')
+                self.import_Screens(temp_group, 'ciclo')
                 self.import_Screens(alarm_group, 'alarm')
+                self.import_Screens(rb1_group, 'RB1')
                 self.import_tags(system_group_tag)
 
     def import_Screens(self, group, screen_type):
         screen_paths = {
-            'sistema': r"\\AXIS-SERVER\Users\Axis Server\Documents\xmls\IHM\Screens\01_Sistema",
-            'prod': r"\\AXIS-SERVER\Users\Axis Server\Documents\xmls\IHM\Screens\Produção e tempo ciclo",
-            'alarm': r"\\AXIS-SERVER\Users\Axis Server\Documents\xmls\IHM\Screens\Alarm"
+            'sistema': r"\\AXIS-SERVER\Users\Axis Server\Documents\xmls\IHM\Screens\01_Sistema\01-1_Axis",
+            'prod': r"\\AXIS-SERVER\Users\Axis Server\Documents\xmls\IHM\Screens\01_Sistema\01-2_Ciclo\Contagem Produção",
+            'ciclo': r"\\AXIS-SERVER\Users\Axis Server\Documents\xmls\IHM\Screens\01_Sistema\01-2_Ciclo\Templo Ciclo",
+            'alarm': r"\\AXIS-SERVER\Users\Axis Server\Documents\xmls\IHM\Screens\01_Sistema\01-3-Alarm",
+            'RB1': r"\\AXIS-SERVER\Users\Axis Server\Documents\xmls\IHM\Screens\03_Telas Operacionais\3.4_Robos\3.4.1_RB01"
         }
         
         if screen_type in screen_paths:
