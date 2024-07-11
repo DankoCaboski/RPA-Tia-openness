2ebbd4bf0307baeffc8066349cc36d4747274b5a
dankocaboski <caboskidanko@gmail.com>
2024-07-11 09:45:38 -0300

    Fix: indentation

--- src/openness/controllers/OpennessController.py
+++ src/openness/controllers/OpennessController.py
@@ -39,12 +39,12 @@ class OpennessController:
                 if hardwware_count > 1:
                     self.openness_service.tia.wire_profinet()
                 self.openness_service.tia.import_blocks(blocks_to_import)
-                    redes = self.openness_service.tia.create_IO_System()
-                    self.openness_service.tia.connect_IO_System(hardwware, redes)
-                    self.openness_service.tia.addIORemota(hardwware)
-                    self.openness_service.tia.save_project()
-                    self.openness_service.tia.import_libraries()
-                    self.openness_service.tia.import_graphics()
+                redes = self.openness_service.tia.create_IO_System()
+                self.openness_service.tia.connect_IO_System(hardwware, redes)
+                self.openness_service.tia.addIORemota(hardwware)
+                self.openness_service.tia.save_project()
+                self.openness_service.tia.import_libraries()
+                self.openness_service.tia.import_graphics()
             self.openness_service.tia.save_project()
             status = "Projeto criado com sucesso!"
             return status
