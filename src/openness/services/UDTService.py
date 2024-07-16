01cdffcda7a0b15e6b86485526c7956e77cee77e
dankocaboski <caboskidanko@gmail.com>
2024-07-16 12:01:43 -0300

    Feat: import_rb_db

--- src/openness/services/UDTService.py
+++ src/openness/services/UDTService.py
@@ -21,3 +21,19 @@ class UDTService:
                 udts.append(match)
         print(f"UDTs: {udts}")
         return udts
+    
+    def list_udt_from_db(self, bk_path):
+        is_valid_path = bk_path and isinstance(bk_path, str) and bk_path.endswith('.xml') and os.path.exists(bk_path) and os.path.isfile(bk_path)
+        if not is_valid_path:
+            raise Exception(f"\nbk_path is not valid\nbk_path: {bk_path}")
+        
+        print(f"\nListing UDTs from {bk_path}")
+        udts = []
+        with open(bk_path, 'r', encoding='utf-8') as file:
+            conteudo = file.read()
+            pattern = r'<Member[^>]+Datatype="&quot;([^&]+)&quot;"[^>]*>'
+
+            matches = re.findall(pattern, conteudo)
+            for match in matches:
+                udts.append(match)
+        return udts
